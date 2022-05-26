f98188091c272c42fdea60c97eb7c97c5379a370
cerna234 <cerna2424@gmail.com>
2022-05-26 06:39:30 -0700

    Server Setup -- Routes and Models, Sequelize Orm setup, Database Configuration

--- client/src/components/Drivers.js
+++ client/src/components/Drivers.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react";
+
 
 function Drivers() {
+
+  const [driversList, Drivers] = useState([]);
+
+  axios.get( "http://localhost:3002/Drivers/").then( (response) => {
+      Drivers(response.data);
+  }, [] );
 
   
   return (
     <div className='Drivers'>
         
-     
+     {driversList.map( (value,key) => {
+       return(
+         <div>{value.Name}</div>
+       )
+     })}
         
     </div>
   )
