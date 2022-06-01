035757d842217b34898ff34806ae320ea7f6739a
cerna234 <cerna2424@gmail.com>
2022-05-31 23:36:21 -0700

    Navbar Update, Driver Component Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
 import './App.css';
-
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 
 import Home from "./Pages/Home"
+import Sponsors from "./Pages/Sponsors"
+import Teams from "./Pages/Teams"
+import AllDrivers from "./Pages/AllDrivers"
 
 function App() {
   return (
@@ -14,8 +15,14 @@ function App() {
         
         <Routes>
 
-
+            
             <Route path="/" exact element={<Home/>} />
+            <Route path="/about" exact element={<Sponsors/>} />
+            <Route path="/drivers" exact element={<AllDrivers/>} />
+            <Route path="/teams" exact element={<Teams/>} />
+
+         
+            
         </Routes>
 
       </Router>
