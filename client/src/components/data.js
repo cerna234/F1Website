e1eceddee90b077fb259362e75f8a6274bedf09b
cerna234 <cerna2424@gmail.com>
2022-06-07 16:31:17 -0700

    implemented scroller for landing page

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -59,3 +59,37 @@ export const sponsorsRow3 = [
 ]
     
 
+export const newsData = [
+    {
+
+        Topic: "Gaming",
+        Title:"F1 MANAGER 2022: Pre-order information, release date and gameplay trailer revealed",
+        Subtext:"Frontier Developments' Formula 1 management sim, F1 Manager 2022, will launch in late August – with pre-orders available now",
+        Img:"https://d2n9h2wits23hf.cloudfront.net/image/v1/static/6057949432001/5f3339f9-a7b8-438c-a75f-33effaa4e659/0d8cff23-4087-43e4-9c4d-0a484838b324/1280x720/match/image.jpg?Key-Pair-Id=APKAINLZLPQXMZG2IC5A&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kMm45aDJ3aXRzMjNoZi5jbG91ZGZyb250Lm5ldC9pbWFnZS92MS9zdGF0aWMvNjA1Nzk0OTQzMjAwMS81ZjMzMzlmOS1hN2I4LTQzOGMtYTc1Zi0zM2VmZmFhNGU2NTkvMGQ4Y2ZmMjMtNDA4Ny00M2U0LTljNGQtMGE0ODQ4MzhiMzI0LzEyODB4NzIwL21hdGNoL2ltYWdlLmpwZyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2NTQ2MzcxNzl9fX1dfQ__&Signature=YoLTuyd3Usg9b8mDXdQc2zBA4yRXT2TJikg5BoYgy6oa3iI~6UuEnpCk6E7i2b6gJSFJ-YGCdvObYLG0ghJ02UO6j~nM4vyEnqhJH8y2DAcch4acVuRFii3Xecim3X2pefyrOjepzqIgyOV9yj8zC5K9Z30qUnNUL3Dja-8VPEUpg9fEZLvuM5m6sstU9LXMk-g8prc7AUniXSZK5QiSrXoTFStgrLD~-1lDauyB-06gLGr5tfxH5x-N-PoaflL5V24rxLwxPrWr4OXlqGzNB0RvJnhZkKtHzS~mwr5uI78wZZAJ6AlrcXV9ZemovQYgl71S32D3Ziuz8abeiXrKVQ__",
+        
+    },
+
+    {
+        Topic: "News",
+        Title:"Williams fined $25,000 for procedural breach of financial regulations",
+        Subtext:"Williams have been fined $25,000 following a procedural breach of Formula 1’s financial regulations, the sport’s governing body the FIA announced on Tuesday.",
+        Img:"https://www.formula1.com/content/dam/fom-website/manual/Misc/2022manual/2022Races/AzerbaijanGP/Pre/WilliamsF1_61828_HiRes.jpg.transform/9col-retina/image.jpg",
+        
+    },
+    {
+        Topic: "Championship",
+        Title:"Checo Pérez wins Monaco Grand Prix in big day for Mexican auto racing",
+        Subtext:"He's now the most successful Mexican F1 driver in history",
+        Img:"https://img.redbull.com/images/q_auto,f_auto/redbullcom/2021/6/6/btoguklkns4wjc81rqau/sergio-perez-azerbaijan-grand-prix-2021",
+        
+    },
+    {
+        Topic: "Racing",
+        Title:"'We made mistakes in our judgement' says Binotto after Ferrari lose out on Monaco victory",
+        Subtext:"Ferrari Team Principal Mattia Binotto has admitted that the team made a mistake with Charles Leclerc's strategy in Monaco, after he lost the lead and finished fourth.",
+        Img:"https://www.formula1.com/content/dam/fom-website/sutton/2021/Hungary/Sunday/1331785177.jpg",
+        
+    }
+
+
+]
